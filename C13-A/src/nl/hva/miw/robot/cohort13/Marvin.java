6fb933d9cc8f38b8f7b8d3459f2d8173d229ba0e
RJReinders <reinder.reinders@hva.nl>
2018-10-17 13:14:17 +0200

    main geleegd

--- C13-A/src/nl/hva/miw/robot/cohort13/Marvin.java
+++ C13-A/src/nl/hva/miw/robot/cohort13/Marvin.java
@@ -1,18 +1,7 @@
 package nl.hva.miw.robot.cohort13;
 
 // imports
-import lejos.hardware.Brick;
-import lejos.hardware.Button;
-import lejos.hardware.Key;
-import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.TextLCD;
-import lejos.hardware.motor.Motor;
-import lejos.hardware.port.SensorPort;
-import lejos.utility.Delay;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.EV3TouchSensor;
-import lejos.robotics.SampleProvider;
-import models.*;
+
 
 public class Marvin {
 
@@ -30,26 +19,7 @@ public class Marvin {
 	}
 
 	private void run() {
-		// start of run
-
-		TextLCD display = brick.getTextLCD();
-		display.drawString("Drukknop test", 0, 3);
-		display.drawString("Druk op enter.", 0, 4);
-		waitForKey(Button.ENTER);
-
-		TouchStop stopknop = new TouchStop();
-		stopknop.start();
-		for (int i = 0; i < 600; i++) {
-
-			display.clear();
-
-			display.drawString("False", 0, 0);
-
-			Delay.msDelay(100);
-		}
-
-		stopknop.endThread();
-		// end of run
+		
 	}
 
 	public void waitForKey(Key key) {
