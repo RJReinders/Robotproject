fb16800b8c2e1f637a77a01af3a458917bc2e2bf
Herman Kaldeway <hfkaldeway@outlook.com>
2018-10-17 13:33:48 +0200

    Classes Assignment en LineFollower toegevoegd

---
+++ C13-A/src/assignments/Assignment.java
@@ -0,0 +1,11 @@
+package assignments;
+
+public abstract class Assignment {
+
+	protected Assignment() {
+		
+	}
+	
+	public abstract void run();
+	
+}
---
+++ C13-A/src/assignments/LineFollower.java
@@ -0,0 +1,70 @@
+package assignments;
+
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+
+public class LineFollower extends Assignment {
+
+	
+	private final int DEFAULT_SPEED = 50;
+	private int min = 12;
+	private int max = 40;
+	private float currentLightIntensity;
+	// private int Mspeed = 10;
+	private int Mspeed2 = 10;
+
+	public LineFollower() {
+		
+	}
+	
+	public void run() {
+	
+		Motor.A.setSpeed(DEFAULT_SPEED);
+		Motor.B.setSpeed(DEFAULT_SPEED);
+
+		EV3ColorSensor colorSensor = new EV3ColorSensor(SensorPort.S3);
+		SampleProvider sp = colorSensor.getRedMode();
+		float[] lightIntensity = new float[sp.sampleSize()];
+	
+		int i = 0;
+	
+		while (i < 5000) {
+			Motor.A.forward();
+			Motor.B.forward();
+		
+			sp.fetchSample(lightIntensity, 0);
+			currentLightIntensity = lightIntensity[0] * 100;
+
+			float motorSpeedA = Mspeed2 * (currentLightIntensity - min);
+			float motorSpeedB = Mspeed2 * (max - currentLightIntensity);
+		
+			if (motorSpeedA < 0) {
+				Motor.A.backward();
+				motorSpeedA = -motorSpeedA * 3;
+			} else {
+				Motor.A.forward();
+			}
+
+			if (motorSpeedB < 0) {
+				Motor.B.backward();
+				motorSpeedB = -motorSpeedB * 3;
+			} else {
+				Motor.B.forward();
+			}
+		
+			Motor.A.setSpeed(motorSpeedA);
+			Motor.B.setSpeed(motorSpeedB);
+		
+			System.out.println(currentLightIntensity);
+		
+			i++;
+		}
+	
+		Motor.A.stop();
+		Motor.B.stop();
+	
+	}
+	
+}
