da7ed83f7706ec449a03d1c12e4fc7a595ae40d4
Paul van Lochem <paul.van.lochem@hva.nl>
2018-10-18 16:59:40 +0200

    Geluidss testen

--- C13-A/src/models/Lights.java
+++ C13-A/src/models/Lights.java
@@ -1,6 +1,9 @@
 package models;
 
+import java.io.File;
+
 import lejos.hardware.Button;
+import lejos.hardware.Sound;
 import lejos.utility.Delay;
 
 public class Lights {
@@ -13,7 +16,8 @@ public class Lights {
 
 		Button.LEDPattern(pattern);
 		Delay.msDelay(delay);
-		
+		File wavFile = new File("we.wav");
+		Sound.playSample(wavFile, 60);
 	}
 
 }
