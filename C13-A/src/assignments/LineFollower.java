24297bcb130be4dbecc901ab087da3dcc0aec3af
Herman Kaldeway <hfkaldeway@outlook.com>
2018-10-18 17:07:12 +0200

    op hoop van zegen

--- C13-A/src/assignments/LineFollower.java
+++ C13-A/src/assignments/LineFollower.java
@@ -15,8 +15,8 @@ public class LineFollower extends Assignment {
 	// attributes: engine
 	private final int DEFAULT_SPEED = 50;
 	private final int DEVIATION = 7;
-	private static int white = 0;
-	private static int black = 100;
+	private static int white = 100;
+	private static int black = 0;
 	private int min = black + DEVIATION;
 	private int max = white - DEVIATION;
 	private int currentLightIntensity;
@@ -30,40 +30,29 @@ public class LineFollower extends Assignment {
 	// attributes: roadmap
 	private static ArrayList<Float> roadMapA = new ArrayList<>();
 	private static ArrayList<Float> roadMapB = new ArrayList<>();
-	
+
 	FindBlueLine findBlueLine = new FindBlueLine(colorSensor);
-	
+
 	public LineFollower() {
 
 	}
-		
+
 	@Override
 	public void run() {
-		
+		calibrateColors();
+		rotateBackToBlackLine();
 		followLine();
-	
 	}
 
 	public void followLine() {
 		Motor.A.setSpeed(DEFAULT_SPEED);
 		Motor.B.setSpeed(DEFAULT_SPEED);
 
-		
-		
-		// Calibration program
-		calibrateColors();
-		rotateBackToBlackLine();
-
-		// Line Follower Program
 		int i = 0;
-		
+
 		findBlueLine.start();
-		
+
 		while (!findBlueLine.getFinished()) {
-
-			Motor.A.setSpeed(50);
-			Motor.B.setSpeed(50);
-
 			Motor.A.forward();
 			Motor.B.forward();
 
@@ -76,10 +65,9 @@ public class LineFollower extends Assignment {
 			roadMapA.add(motorSpeedA);
 			roadMapB.add(motorSpeedB);
 
-			
-			//System.out.println(roadMapA.get(i));
-			//System.out.println(roadMapB.get(i));
-			
+			// System.out.println(roadMapA.get(i));
+			// System.out.println(roadMapB.get(i));
+
 			if (motorSpeedA < 0) {
 				Motor.A.backward();
 				motorSpeedA = -motorSpeedA * 4;
@@ -98,22 +86,16 @@ public class LineFollower extends Assignment {
 			Motor.B.setSpeed(motorSpeedB);
 
 			Delay.msDelay(100);
-			
+
 			// System.out.println(currentLightIntensity);
 		}
-		
+
 		System.out.println("Tracktime = " + findBlueLine.getTrackTime());
-	
+
 		findBlueLine.endThread();
-	
-		Motor.A.stop();
-		Motor.B.stop();
-
-			i++;
 
 		Motor.A.stop();
 		Motor.B.stop();
-
 	}
 
 	private void rotateBackToBlackLine() {
@@ -121,41 +103,20 @@ public class LineFollower extends Assignment {
 		Motor.B.forward();
 		Motor.A.setSpeed(DEFAULT_SPEED);
 		Motor.B.setSpeed(DEFAULT_SPEED);
-		
+
 		sp.fetchSample(lightIntensity, 0);
 		currentLightIntensity = (int) (lightIntensity[0] * 100);
-		
-		//TODO terugdraaien tot je zwart hebt gescand ipv code hieronder
-		/*
-		float motorSpeedA = speedFactor * (currentLightIntensity - min);
-		float motorSpeedB = speedFactor * (max - currentLightIntensity);
 
-		if (motorSpeedA < 0) {
-			Motor.A.backward();
-			motorSpeedA = -motorSpeedA * 4;
-		} else {
-			Motor.A.forward();
-		}
-
-		if (motorSpeedB < 0) {
-			Motor.B.backward();
-			motorSpeedB = -motorSpeedB * 4;
-		} else {
-			Motor.B.forward();
-		}
-
-		Motor.A.setSpeed(motorSpeedA);
-		Motor.B.setSpeed(motorSpeedB);
-
-		Delay.msDelay(100);
-		*/
+		// TODO terugdraaien tot je zwart hebt gescand ipv code hieronder
 	}
 
 	public void calibrateColors() {
+
 		// local variables
 		ArrayList<Float> calibrationValues = new ArrayList<>();
 		colorSensor.setCurrentMode("Red");
 		boolean testingDone = false;
+
 		// making test readings
 		while (!testingDone) {
 			// add test sample
@@ -167,13 +128,14 @@ public class LineFollower extends Assignment {
 			Motor.A.setSpeed(100);
 			Motor.B.setSpeed(100);
 			Delay.msDelay(250);
+
 			// continue until number of samples is collected
 			if (calibrationValues.size() >= 25) {
 				testingDone = true;
 				Sound.beep();
-			}				
+			}
 		}
-		
+
 		// set the black(est) and white(st) values
 		for (int i = 0; i < calibrationValues.size(); i++) {
 			if (calibrationValues.get(i) < black)
@@ -181,18 +143,18 @@ public class LineFollower extends Assignment {
 			if (calibrationValues.get(i) > white)
 				white = calibrationValues.get(i).intValue();
 		}
+
 		// print the values (testcode: kan later weg)
 		System.out.println("Zwartwaarde: " + black);
 		System.out.println("Witwaarde: " + white);
-		
-		//TODO: toevoegen check of er wel wit en zwart is gemeten
-		
+
+		// TODO: toevoegen check of er wel wit en zwart is gemeten
+
 		// reset motors
 		Motor.A.stop();
 		Motor.B.stop();
-
 	}
-	
+
 	public static ArrayList<Float> getRoadMapA() {
 		return roadMapA;
 	}
